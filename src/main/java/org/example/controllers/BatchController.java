52fc88f9c1df71640baf237f0680056818a2ccd0
Vladislav Osada <Osada.0339@gmail.com>
2023-08-06 14:16:22 +0300

    Пакетное обновление

---
+++ src/main/java/org/example/controllers/BatchController.java
@@ -0,0 +1,39 @@
+package org.example.controllers;
+
+import org.example.dao.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Vladislav Osada
+ * @date 06.08.2023 13:45
+ */
+@Controller
+@RequestMapping("/test-batch")
+public class BatchController {
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public BatchController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping
+    public String index(){
+        return "batch/index";
+    }
+
+    @GetMapping("/without")
+    public String withoutBatch(){
+        personDAO.multipleUpdate();
+        return "redirect:/people";
+    }
+
+    @GetMapping("/with")
+    public String withBatch(){
+        personDAO.testBatchUpdate();
+        return "redirect:/people";
+    }
+}
--- src/main/java/org/example/dao/PersonDAO.java
+++ src/main/java/org/example/dao/PersonDAO.java
@@ -2,10 +2,14 @@ package org.example.dao;
 
 import org.example.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -30,7 +34,7 @@ public class PersonDAO {
     }
 
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person VALUES (1,?,?,?)", person.getName(), person.getAge(), person.getEmail());
+        jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES (?,?,?)", person.getName(), person.getAge(), person.getEmail());
     }
 
     public void update(int id, Person updatedPerson) {
@@ -40,5 +44,58 @@ public class PersonDAO {
 
     public void delete(int id) {
         jdbcTemplate.update("DELETE  FROM Person Where id=?", id);
+    }
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////Пакентное обновление}
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public void multipleUpdate() {
+        List<Person> people = create1000People();
+
+        long before = System.currentTimeMillis();
+
+        for (Person person : people
+        ) {
+            jdbcTemplate.update("INSERT INTO person VALUES (?,?,?,?)", person.getId(), person.getName(), person.getAge(), person.getEmail());
+        }
+
+        long after = System.currentTimeMillis();
+
+        System.out.println("Множественное добавление заняло " + (after - before));
+    }
+
+    public void testBatchUpdate() {
+        List<Person> people = create1000People();
+        long before = System.currentTimeMillis();
+
+        jdbcTemplate.batchUpdate("INSERT INTO person VALUES (?,?,?,?)", new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+                preparedStatement.setInt(1, people.get(i).getId());
+                preparedStatement.setString(2, people.get(i).getName());
+                preparedStatement.setInt(3, people.get(i).getAge());
+                preparedStatement.setString(4, people.get(i).getEmail());
+            }
+
+            @Override
+            public int getBatchSize() {
+                return people.size();
+            }
+        });
+
+        long after = System.currentTimeMillis();
+
+        System.out.println("Время затраченное пакетным обновленнием " + (after - before));
+    }
+
+    private List<Person> create1000People() {
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+            people.add(new Person(i, "name" + i, 20, "test" + i + "email.com"));
+        }
+
+        return people;
     }
 }
